0024a453fbaf432f7b6b23918a16aa375f2cbfb5
ianMuchesia <ianmuchesia@yahoo.com>
2024-03-15 13:39:50 +0300

    next computer

--- client/src/components/contacts/ContactForm.tsx
+++ client/src/components/contacts/ContactForm.tsx
@@ -1,98 +1,82 @@
 import Button from "@/UI/Button";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
-
+interface FormInputs {
+  name: string;
+  email: string;
+  comment: string;
+}
 const ContactForm = () => {
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    tel: "",
-    message: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormInputs>();
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setForm((prevForm) => {
-      const { name, value } = e.target;
-      return {
-        ...prevForm,
-        [name]: value,
-      };
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const { name, email, tel, message } = form;
-    if (!name || !email ||  !message) {
-      toast.warn("please fill all the inputs");
-      return;
-    }
+  const onSubmit = async (data: FormInputs) => {
     try {
       const response = await fetch(
-      
-        "https://formsubmit.co/91cef90dd867a8bd0fdd26004c6a4cce",
+        "https://formsubmit.io/send/0to60motoringkenya@gmail.com",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(form),
-       
+          body: JSON.stringify(data),
+          mode: "no-cors"
         }
       );
+
       if (response.ok) {
-        toast.success("sent successfully");
-        setForm({
-          name: "",
-          email: "",
-          tel: "",
-          message: "",
-        });
+        toast.success("Sent successfully");
+        reset();
+      } else {
+        console.log(response)
+        throw new Error("Network response was not ok");
       }
-    } catch (error: any) {
-      toast.error("something wrong happened, try again later");
-      console.log(error);
+    } catch (error) {
+      toast.error("An error occurred");
+      console.log(error)
     }
   };
-
   return (
     <form
       className="flex flex-col py-6 space-y-6 md:py-0 md:px-6"
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <label className="block">
         <span className="mb-1">Full name</span>
+
         <input
-          type="text"
+          {...register("name", { required: true })}
           placeholder="Your Name"
           className="block w-full rounded-md shadow-sm px-4 py-2 focus:outline-none "
-          value={form.name}
-          name="name"
-          onChange={handleChange}
         />
+        {errors.name && <p className="text-darkRed text-sm">Your name is required</p>}
       </label>
       <label className="block">
         <span className="mb-1">Email address</span>
+
         <input
-          type="email"
+          {...register("email", { required: true })}
           placeholder="Enter Your Email Address"
           className="block w-full rounded-md shadow-sm px-4 py-2 focus:outline-none"
-          value={form.email}
-          name="email"
-          onChange={handleChange}
         />
+        {errors.email && <p className="text-darkRed text-sm">Email address is required</p>}
       </label>
       <label className="block">
         <span className="mb-1">Message</span>
+
         <textarea
           rows={5}
           className="block w-full rounded-md shadow-sm px-4 py-2 focus:outline-none "
-          value={form.message}
-          name="message"
-          onChange={handleChange}
+          {...register("comment", { required: true })}
+          placeholder="Your Message"
+
         />
+        {errors.comment && <p className="text-darkRed text-sm">Message is required</p>}
       </label>
       <Button type="submit" text="SUBMIT" />
     </form>
