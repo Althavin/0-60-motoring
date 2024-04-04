ab002e0f46480a07ccd895a32585cb11ef2d3717
ianMuchesia <ianmuchesia@yahoo.com>
2024-04-04 13:52:45 +0300

    new date

--- client/src/components/blogs/Article.tsx
+++ client/src/components/blogs/Article.tsx
@@ -9,7 +9,16 @@ interface Props{
 
 }
 const Article = ({blog}:Props) => {
-    
+    const formatDate = (date: any) => {
+        if (date) {
+          const dt = new Date(date);
+          const month =
+            dt.getMonth() + 1 < 10 ? "0" + (dt.getMonth() + 1) : dt.getMonth() + 1;
+          const day = dt.getDate() < 10 ? "0" + dt.getDate() : dt.getDate();
+          return day + "/" + month + "/" + dt.getFullYear();
+        }
+        return "";
+      };
   return (
     <article className="p-6 bg-white rounded-lg border border-red shadow-md " >
     <div className="flex justify-between items-center mb-5 text-blue-500">
@@ -17,7 +26,7 @@ const Article = ({blog}:Props) => {
             <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
             Article
         </span>
-        <span className="text-sm">6 March</span>
+        <span className="text-sm">{formatDate(blog.createdAt)}</span>
     </div>
     <h2 className="mb-2 text-2xl font-bold tracking-tight text-blue-700  min-h-[3em]"><a href="#">{blog.title}</a></h2>
     <p className="mb-5 font-light text-gray-500 ">
